33715c2cbc3aa643e074fde2090439ade5dc92e0
Augusto Polonio <augustodrp@gmail.com>
2024-10-14 12:31:01 -0300

    feat: loading player assets

--- game.js
+++ game.js
@@ -1,9 +1,26 @@
+let player;
+
 // Game scene
 class SpaceShooter extends Phaser.Scene {
   preload() {
+    const stpriteConfig = {
+      frameWidth: 106,
+      frameHeight: 80      
+    }
+
+    this.load.spritesheet('player', 'assets/player.png', stpriteConfig)
   }
 
   create() {
+    player = this.physics.add.sprite(
+      this.scale.width * 0.5,
+      this.scale.height - 50, 
+      'player'
+    );
+    player.depth = 1;
+    player.setCollideWorldBounds(true);
+
+    
   }
 
   update(ts, dt) {
